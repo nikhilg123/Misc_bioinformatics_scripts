12b55c32bf2300758d9efef24abf723b90e07e13
nikhilg123 <32106363+nikhilg123@users.noreply.github.com>
2024-11-18 22:55:46 -0500

    improve readability of genome_summary_stats.py

--- genome_summary_stats.py
+++ genome_summary_stats.py
@@ -1,5 +1,6 @@
 #this script is to perform genome QC on all isolate genomes obtained from a collaborator
-#output (currently prints to stdout) includes the name of the genome, the number of sequences that make up the genome, the %GC, N50, the shortest and longest sequence, and the total amount of nucleotides/bp in the genome
+#output (currently prints to stdout) includes the name of the genome, the number of sequences that make up the genome,
+#the %GC, N50, the shortest and longest sequence, and the total amount of nucleotides/bp in the genome
 #unlike v2, this implements parallelization using concurrent.futures library for a 2-3 fold speed up
 
 from Bio import SeqIO
@@ -32,13 +33,20 @@ def assess_genome_quality(file_path):
 		number_of_sequences += 1
 		sequences.append(str(sequence.seq)) #only the sequence, i.e., no header, is being appended
 	sequence_lengths = [len(sequence) for sequence in sequences] #list comprehension to get a list of all sequence lengths
-	sequence_lengths.sort(reverse=True) #sorting occurs in place, which allows the shortest and longest sequences to be easily retrieved, and aids the calculation of N50
+	sequence_lengths.sort(reverse=True) #sorting occurs in place, which allows the shortest and longest sequences to be easily
+	#retrieved, and aids the calculation of N50
 	total_bp = sum(sequence_lengths)
 	nucleotide_counts = Counter("".join(sequences)) #all sequences within genome are concatenated and characters are counted 
 	GC = ((nucleotide_counts.get('C') + nucleotide_counts.get('G'))/total_bp)*100 #calculating GC content using output from Counter
 	try:
-		#this ensures that genomes with ambigious nucleotides (aside from "N") are removed. Other genomes will still run as we are spawning and running multiple processes with concurrent.futures
-		assert total_bp == nucleotide_counts.get('C') + nucleotide_counts.get('G') + nucleotide_counts.get('T') + nucleotide_counts.get('A') + nucleotide_counts.get('N'), f"Assertion Error: {genome_name}.fasta contains invalid characters and was ignored"
+		#this ensures that genomes with ambigious nucleotides (aside from "N") are removed. Other genomes will still run as we 
+		#are spawning and running multiple processes with concurrent.futures
+		assert total_bp == (nucleotide_counts.get('C') +
+		nucleotide_counts.get('G') +
+		nucleotide_counts.get('T') + 
+		nucleotide_counts.get('A') +
+		nucleotide_counts.get('N')
+		), f"Assertion Error: {genome_name}.fasta contains invalid characters and was ignored"
 	except AssertionError as e:
 			time.sleep(5)       
 			print (e)
@@ -54,11 +62,17 @@ def assess_genome_quality(file_path):
 	#currently output prints to stdout
 	#need to revisit print formatting, perhaps implement as dataframe
 	#need to revisit formatting and write to an outfile so stdout doesn't need to be redirected to file within shell		
-	print(f"genome name:{genome_name}\t number of sequences:{number_of_sequences:,}\t GC:{GC:.2f}%\t N50:{N50:,}\t longest sequence:{sequence_lengths[0]:,}\t shortest sequence:{sequence_lengths[-1]:,}\t total bp:{total_bp:,}")
+	print(f"genome name:{genome_name}\t"
+	      f"number of sequences:{number_of_sequences:,}\t"
+	      f"GC:{GC:.2f}%\t N50:{N50:,}\t"
+	      f"longest sequence:{sequence_lengths[0]:,}\t"
+	      f"shortest sequence:{sequence_lengths[-1]:,}\t"
+	      f"total bp:{total_bp:,}"
+	     )
 
 def main():
-	fasta_files = [files for files in glob.glob(f"{args.file_path}/*.fasta") if "GD" in files] #GD is an internal keyword. Modify or remove as needed.
-	#print(fasta_files)								 
+	fasta_files = [files for files in glob.glob(f"{args.file_path}/*.fasta") if "GD" in files] #GD is an internal keyword
+	#Modify or remove as needed.							 
 	with concurrent.futures.ProcessPoolExecutor() as executor: 
 		executor.map(assess_genome_quality, fasta_files) #calls the function "assess_genome_quality" for each element in "fasta_files"
 
